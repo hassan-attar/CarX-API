7ed451f51e8824b39336b33f7ab6f89648974981
Hassan Attar <info@hassanattar.com>
2024-08-10 18:16:21 -0700

    hooked up the account controllers to account router
    Signed-off-by: Hassan Attar <info@hassanattar.com>

--- src/routes/accounts/index.ts
+++ src/routes/accounts/index.ts
@@ -1,9 +1,43 @@
 import { Router } from 'express';
-import {getProfile} from "../../controllers/accounts";
+import {getProfile, patchProfile} from "../../controllers/accounts";
 import isAuthenticatedMiddleware from "../../middlewares/isAuthenticatedMiddleware";
+import multer from "multer";
+import InvalidFileTypeError from "../../errors/ClientError/InvalidFileTypeError";
+import path from "node:path";
+
+const allowedTypes = ['image/jpeg', 'image/png'];
+
+const storage = multer.diskStorage({
+    destination: function (__, _, cb) {
+        console.log("1")
+        cb(null, path.join(__dirname, "..", "..", "..", "tmp"))
+    },
+    filename: function (_, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+})
+const upload = multer({
+    storage: storage,
+    limits: {
+        files: 1,
+        fileSize: 5 * 1024 * 1024,
+    },
+    fileFilter(_, file: Express.Multer.File, callback: multer.FileFilterCallback) {
+        if (!allowedTypes.includes(file.mimetype)) {
+            return callback(new InvalidFileTypeError({
+                details: [{
+                    message: "supported format for images: image/jpeg, image/png"
+                }]
+            }));
+        }
+        callback(null, true);
+    }
+})
 
 const router = Router();
 router.use(isAuthenticatedMiddleware)
 router.get("/profile", getProfile)
+router.patch("/profile", upload.single("profileImage"), patchProfile)
 
 export default router;
