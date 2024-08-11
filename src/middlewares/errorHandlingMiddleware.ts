78cabde70b05daa1338aceb58ae3e69e804ae90d
Hassan Attar <info@hassanattar.com>
2024-08-10 18:12:44 -0700

    updated error handling middleware to support api errors
    Signed-off-by: Hassan Attar <info@hassanattar.com>

--- src/middlewares/errorHandlingMiddleware.ts
+++ src/middlewares/errorHandlingMiddleware.ts
@@ -2,8 +2,60 @@ import {ErrorRequestHandler} from "express";
 import {BaseError} from "../errors/BaseError/BaseError";
 import ClientError from "../errors/ClientError/ClientError";
 import {StatusCodes} from "http-status-codes";
+import {MulterError} from "multer";
+import ValidationError from "../errors/ClientError/ValidationError";
+
+const bodyParserCommonErrorsTypes = [
+    "encoding.unsupported",
+    "entity.parse.failed",
+    "entity.verify.failed",
+    "request.aborted",
+    "request.size.invalid",
+    "stream.encoding.set",
+    "parameters.too.many",
+    "charset.unsupported",
+    "entity.too.large",
+];
+function isBodyParserError(error: any) {
+    return bodyParserCommonErrorsTypes.includes(error.type);
+}
+
 
 const errorHandlingMiddleware:ErrorRequestHandler = (err , __ , res , _ ) => {
+    console.error("ErrorHandlingMiddleware: ", err)
+    if(err instanceof MulterError){
+        if(err.code === "LIMIT_FILE_SIZE"){
+            err = new ValidationError({
+                message: "File size has exceeded the limit, please reduce the file size or use different file.",
+                details: [{
+                    "message": "Maximum file size allowed is 5MB."
+                }]
+            })
+        }else if(err.code === "LIMIT_PART_COUNT"){
+            err = new ValidationError({
+                message: "Too many files uploaded"
+            })
+        }else if(err.code === "LIMIT_FIELD_COUNT"){
+            err = new ValidationError({
+                message: "The field for file is not supported"
+            })
+        }else{
+            err = new ValidationError({
+                message: "error in validating the file"
+            })
+        }
+    }
+    if(isBodyParserError(err)){
+        console.log(err.expose)
+        console.log(err.message)
+        console.log(err.body)
+        return res.status(err.statusCode).json({
+            error: err.type,
+            message: err.expose ? err.message : "Something went wrong with parsing your payload! Please try again later.",
+            details: []
+        })
+    }
+
     if (!(err instanceof BaseError)) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
             error: 'InternalServerError',
